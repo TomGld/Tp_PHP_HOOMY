0cd5651e8310be3c075ef1c172f8bcfad8a9c382
Mayer MICHEL <mayer.mohib@gmail.com>
2025-04-15 15:15:47 +0200

    add Groups

--- www/src/Entity/SettingData.php
+++ www/src/Entity/SettingData.php
@@ -2,31 +2,59 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
-use App\Repository\SettingDataRepository;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\Get;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use App\Repository\SettingDataRepository;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: SettingDataRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    //autorisation des route que l'on veut acceder
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Patch()
+    ],
+    normalizationContext: ['groups' => ['settingData:read']],
+    denormalizationContext: ['groups' => ['settingData:write']]
+)]
+
+#[ApiFilter(
+    SearchFilter::class,
+    properties: [
+        'name' => 'iexact',
+        'id' => 'exact',
+    ]
+)]
 class SettingData
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['settingData:read'])]
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'settingData')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['settingData:read'])]
     private ?SettingType $settingType = null;
 
     #[ORM\Column(length: 25)]
+    #[Groups(['settingData:read', 'settingData:write'])]
     private ?string $data = null;
 
     #[ORM\ManyToOne(inversedBy: 'settingData')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['settingData:read'])]
     private ?Vibe $vibe = null;
 
     #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[Groups(['settingData:read', 'device:read'])]
     private ?Device $device = null;
 
     public function getId(): ?int
